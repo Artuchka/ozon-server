83e5830e2bb57fb50d0ec44b0fc76ec169c06da5
JohnDoeee <yandex949@gmail.com>
2022-12-19 12:37:03 +0300

    \w no httpOnly

--- utils/cookies.js
+++ utils/cookies.js
@@ -2,7 +2,7 @@ const attachCookies = async ({ res, token }) => {
 	const oneDay = 1000 * 60 * 60 * 24
 	res.cookie("token", token, {
 		expires: new Date(Date.now() + oneDay),
-		httpOnly: false,
+		// httpOnly: false,
 		sameSite: "none",
 		secure: true,
 	})
