47f3408c24fa27cfb6fc6317dac5accd0d273942
Temka Artemka <yandex949@gmail.com>
2023-01-26 00:38:34 +0300

    factor: prior to Dadata for street geodecoding + api keys in env

--- utils/location.js
+++ utils/location.js
@@ -1,45 +1,51 @@
 const { default: axios } = require("axios")
+const fetch = require("node-fetch")
 
-const api_key = "45d95055-c372-4cc1-9468-637658d3e41a"
 async function getAddressByCoordinates(coords) {
-	const res = await getAddressFromYandex(coords)
-	// if(!res) {
-	// 	await getAddressFromDadata(coords)
-	// }
+	let res = await getAddressFromDadata(coords)
+	if (!res) {
+		res = await getAddressFromYandex(coords)
+	}
 
 	return res
 }
 
+const yandexAPI = process.env.YandexAPIKEY
+const urlYandex = `https://geocode-maps.yandex.ru/1.x/?apikey=${yandexAPI}&format=json&geocode=`
 async function getAddressFromYandex(coords) {
 	const prepCoords = coords.reverse().join(",")
-	const { data } = await axios(
-		`https://geocode-maps.yandex.ru/1.x/?apikey=${api_key}&format=json&geocode=${prepCoords}`,
-		{
-			method: "GET",
-			headers: { "Content-Type": "application/json" },
-		}
-	)
+	const { data } = await axios(`${urlYandex}${prepCoords}`, {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	})
 	const result =
-		data?.response?.GeoObjectCollection?.featureMember[0].GeoObject
+		data?.response?.GeoObjectCollection?.featureMember[0]?.GeoObject
 			?.metaDataProperty?.GeocoderMetaData?.Address?.formatted
-	console.log({ result })
+
 	return result
 }
-// async function getAddressFromDadata (coords)  {
-// 	const prepCoords = coords.reverse().join(",")
-// 	const { data } = await axios(
-// 		`https://geocode-maps.yandex.ru/1.x/?apikey=${api_key}&format=json&geocode=${prepCoords}`,
-// 		{
-// 			method: "GET",
-// 			headers: { "Content-Type": "application/json" },
-// 		}
-// 	)
-// 	const result = (
-// 		data?.response?.GeoObjectCollection?.featureMember[0].GeoObject
-// 			?.metaDataProperty?.GeocoderMetaData?.Address?.formatted
-// 	)
-// 	console.log({result});
-// 	return result
-// }
+
+const urlDadata =
+	"https://suggestions.dadata.ru/suggestions/api/4_1/rs/geolocate/address"
+const dadataToken = process.env.DadataAPIKEY
+async function getAddressFromDadata(coords) {
+	const query = { lat: coords[0], lon: coords[1] }
+
+	const options = {
+		method: "POST",
+		mode: "cors",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+			Authorization: "Token " + dadataToken,
+		},
+		body: JSON.stringify(query),
+	}
+
+	const response = await fetch(urlDadata, options)
+	const data = await response.json()
+
+	return data.suggestions[0].unrestricted_value
+}
 
 module.exports = { getAddressByCoordinates }
