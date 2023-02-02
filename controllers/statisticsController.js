808b958d41c99fa6c1264bca04c4f9de942ca3e6
Temka Artemka <yandex949@gmail.com>
2023-02-02 22:22:03 +0300

    factor:feature: moved totalStats calculations from front to back

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -3,6 +3,7 @@ const { Statistics } = require("../models/StatisticsModel")
 const { checkPermission } = require("../utils/checkPermission")
 const { NotFoundError } = require("../errors/customError")
 const { default: mongoose } = require("mongoose")
+const { Products } = require("../models/productModel")
 
 const getAllMyStatistics = async (req, res) => {
 	const { userId } = req.user
@@ -23,14 +24,48 @@ const getAllMyStatistics = async (req, res) => {
 		},
 	])
 
-	console.log("-----------")
-	console.log(stats)
-	console.log("-----------")
+	const userProducts = await Products.find({ vendor: userId })
+	const productsTotals = getMyProductTotals(userProducts)
 
 	res.status(StatusCodes.OK).json({
 		msg: "all my statistics",
 		stats,
+		productsTotals,
 	})
+}
+
+function getMyProductTotals(products) {
+	let totalPrice = 0
+	let totalAverageRating = 0
+	let totalNumOfReviews = 0
+	let productsAmount = products?.length
+	let totalImagesAmount = 0
+
+	for (let index = 0; index < productsAmount; index++) {
+		const { price, averageRating, numOfReviews, images } = products[index]
+
+		totalPrice += price
+		totalImagesAmount += images?.length
+		totalAverageRating += averageRating
+		totalNumOfReviews += numOfReviews
+	}
+	let averagePrice = totalPrice / productsAmount
+	let averageImagesAmount = totalImagesAmount / productsAmount
+	let averageRating = totalAverageRating / productsAmount
+	let averageNumOfReviews = totalNumOfReviews / productsAmount
+
+	averagePrice = parseFloat(averagePrice.toFixed(2))
+	averageImagesAmount = parseFloat(averageImagesAmount.toFixed(2))
+	averageRating = parseFloat(averageRating.toFixed(2))
+	averageNumOfReviews = parseFloat(averageNumOfReviews.toFixed(2))
+	return {
+		totalPrice,
+		averagePrice,
+		productsAmount,
+		averageImagesAmount,
+		averageRating,
+		averageNumOfReviews,
+	}
 }
 
 const getMySingleStatistics = async (req, res) => {
